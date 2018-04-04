8c7f187704089b991bbb8f1be08f8692fc421857
Brent Krull <bkrull@ksu.edu>
2018-04-04 17:55:26 -0500

    Updates to code -- Plan to rework some stuff and rebase branch

--- vm/page.c
+++ vm/page.c
@@ -53,6 +53,10 @@ page_for_addr (const void *address)
       /* No page.  Expand stack? */
 
 /* add code */
+      if ((p.addr > PHYS_BASE - STACK_MAX) && ((void *)thread_current()->user_esp - 32 < address))
+      {
+        return page_allocate (p.addr, false);
+      }
 
     }
   return NULL;
@@ -146,13 +150,51 @@ page_out (struct page *p)
      page. */
 
 /* add code here */
+  pagedir_clear_page(p->thread->pagedir, (void *) p->addr);
 
   /* Has the frame been modified? */
 
 /* add code here */
+  /* If the frame has been modified, set 'dirty' to true. */
+  dirty = pagedir_is_dirty (p->thread->pagedir, (const void *) p->addr);
+
+  /* If the frame is not dirty (and file != NULL), we have sucsessfully evicted the page. */
+  if(!dirty)
+  {
+    ok = true;
+  }
 
   /* Write frame contents to disk if necessary. */
+  /* If the file is null, we definitely don't want to write the frame to disk. We must swap out the
+     frame and save whether or not the swap was successful. This could overwrite the previous value of
+     'ok'. */
+  if (p->file == NULL)
+  {
+    ok = swap_out(p);
+  }
+  /* Otherwise, a file exists for this page. If file contents have been modified, then they must be
+     be written back to the file system on disk, or swapped out. This is determined by the private
+     variable associated with the page. */
+  else
+  {
+    if (dirty)
+    {
+      if(p->private)
+      {
+        ok = swap_out(p);
+      }
+      else
+      {
+        ok = file_write_at(p->file, (const void *) p->frame->base, p->file_bytes, p->file_offset);
+      }
+    }
+  }
 
+  /* Nullify the frame held by the page. */
+  if(ok)
+  {
+    p->frame = NULL;
+  }
 /* add code here */
 
   return ok;
